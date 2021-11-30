fa1fcf69ff6c6e1ed7e491d17628190654104843
brown <ohbass95@gmail.com>
2021-11-30 14:25:46 +0900

    spring02

--- src/main/java/com/care/root/CookieController.java
+++ src/main/java/com/care/root/CookieController.java
@@ -1,5 +1,26 @@
 package com.care.root;
 
-public class CookieController {
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequestWrapper;
+import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CookieController {
+	@GetMapping("cookie")
+	public String cookie(HttpServletResponse response,
+							HttpServletRequestWrapper req) {
+		Cookie cook = new Cookie ("myCookie", "ÄíÅ°»ý¼º");
+		cook.setMaxAge(5);
+		
+		response.addCookie(cook);
+		
+		for(Cookie c : req.getCookies() ) {
+			System.out.println(c.getName());
+		}
+		
+		return "cook/cookie";
+	}
 }
